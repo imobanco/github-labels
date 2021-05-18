19aab3f4514b2527a12dd4fbd9bb4d9a52fed75a
rodrigondec <rodrigondec@gmail.com>
2021-05-18 11:12:21 -0300

    fmt

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import json
 
 from decouple import config
 
-TOKEN = config('TOKEN')
+TOKEN = config("TOKEN")
 
-with open('labels.json') as json_file:
+with open("labels.json") as json_file:
     LABELS = json.load(json_file)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import TOKEN, LABELS
 
 
 def create_or_update_labels(owner, repo):
-    print('Creating or updating labels!!')
+    print("Creating or updating labels!!")
     g = Github(TOKEN)
 
     repo = g.get_repo(f"{owner}/{repo}")
@@ -14,11 +14,11 @@ def create_or_update_labels(owner, repo):
             repo.create_label(**label)
         except GithubException as e:
             # print(e)
-            repo_label = repo.get_label(label.get('name'))
+            repo_label = repo.get_label(label.get("name"))
             repo_label.edit(
                 name=repo_label.name,
-                color=label.get('color'),
-                description=label.get('description')
+                color=label.get("color"),
+                description=label.get("description"),
             )
         except Exception as e:
             # print(e)
@@ -26,7 +26,7 @@ def create_or_update_labels(owner, repo):
 
 
 def delete_existing_labels(owner, repo):
-    print('Deleting old labels!!!')
+    print("Deleting old labels!!!")
     g = Github(TOKEN)
 
     repo = g.get_repo(f"{owner}/{repo}")
